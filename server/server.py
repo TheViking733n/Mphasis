f2403a47617039686235e25322e6b5bd7b1c0bde
Anant Prakash Singh <anant733n@gmail.com>
2023-12-12 03:52:18 +0530

    created settings route

--- server/server.py
+++ server/server.py
@@ -7,5 +7,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)

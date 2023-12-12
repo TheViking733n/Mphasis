213e18f09340e2532e8c5a8e836d67baab09b2c2
Anant Prakash Singh <anant733n@gmail.com>
2023-12-12 05:34:40 +0530

    added images and removed settings.html

---
+++ server/config.py
@@ -0,0 +1,14 @@
+'''
+This file contains default values for various configuration options.
+
+'''
+
+
+# Input dataset google sheet urls
+INVENTORY_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1abfWjKouFxD_erzhdnam8tCtFC5G_956/edit#gid=2096778584'
+SCHEDULE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/19jmopenGv7VnXOWeBmPpsicmCZycEfOl/edit#gid=2051886986'
+PNR_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1E3Vnx5WA0ntOG42A_oyNI1VCNMn_UY6938mt9Mxz2gs/edit#gid=0'
+PASSENGER_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1SB4k3Rf3dzIskoJfLFByiqjA3SFiSwlj/edit#gid=1455340004'
+RESCHEUDLE_SHEET_URL = ''
+
+
--- server/server.py
+++ server/server.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
+from config import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
 
 
 
